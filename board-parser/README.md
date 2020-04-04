3dd4ab3bf437edf344568a3d06a7a29a04fa9c00
bausano <michael@enhance.com>
2020-04-04 18:30:21 +0100

    Editor config and fmt

---
+++ board-parser/README.md
@@ -0,0 +1,3 @@
+# Board parser
+Given an image, this library attempts to find a go board with stones in it and
+identify position of each stone in the lattice. 
--- board-parser/src/lib.rs
+++ board-parser/src/lib.rs
@@ -1,20 +1,9 @@
-use score_counter::Board;
-use score_counter::Stone;
-
-pub fn parse_image() -> Board<Stone> {
+pub fn parse_image() {
     unimplemented!()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        match parse_image() {
-            Board::NineByNine(board) => assert_eq!(9, board.len()),
-            Board::ThirteenByThirteen(board) => assert_eq!(13, board.len()),
-            Board::NineteenByNineteen(board) => assert_eq!(19, board.len()),
-        }
-    }
+    //
 }
